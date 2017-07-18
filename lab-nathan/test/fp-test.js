fb01aaedb335a33c6ce3c71a841cbcd3d6117a50
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-07-18 13:57:55 -0700

    Use only lexical arrow functions.

--- lab-nathan/test/fp-test.js
+++ lab-nathan/test/fp-test.js
@@ -3,37 +3,37 @@
 const fp = require('../lib/fp.js');
 const expect = require('chai').expect;
 
-describe('fp', function() {
-  describe('#map()', function() {
-    it('should return an array of doubled values.', function() {
+describe('fp', () => {
+  describe('#map()', () => {
+    it('should return an array of doubled values.', () => {
       let result = fp.map([1, 2, 3], num => num * 2);
       expect(result).to.deep.equal([2, 4, 6]);
     });
   });
 
-  describe('#filter()', function() {
-    it('should return 2.', function() {
+  describe('#filter()', () => {
+    it('should return 2.', () => {
       let result = fp.filter([1, 2, 3], num => num !== 2);
       expect(result).to.deep.equal([1, 3]);
     });
   });
 
-  describe('#reduce()', function() {
-    it('should return 6.', function() {
+  describe('#reduce()', () => {
+    it('should return 6.', () => {
       let result = fp.reduce([1, 2, 3], (acc, cur) => acc + cur, 0);
       expect(result).to.equal(6);
     });
   });
 
-  describe('#concat()', function() {
-    it('should return [1, 2, 3, 4, 5, 6].', function() {
+  describe('#concat()', () => {
+    it('should return [1, 2, 3, 4, 5, 6].', () => {
       let result = fp.concat([1, 2, 3], [4, 5, 6]);
       expect(result).to.deep.equal([1, 2, 3, 4, 5, 6]);
     });
   });
 
-  describe('#splice()', function() {
-    it('should return [1, 2, 3, 7, 8, 9, 4, 5, 6].', function() {
+  describe('#splice()', () => {
+    it('should return [1, 2, 3, 7, 8, 9, 4, 5, 6].', () => {
       let result = fp.splice([1, 2, 3, 4, 5, 6], 3, 0, [7, 8, 9]);
       expect(result).to.deep.equal([1, 2, 3, 7, 8, 9, 4, 5, 6]);
     });
