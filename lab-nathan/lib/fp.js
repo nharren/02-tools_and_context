fb5c6e65b92b57a64a72ed4ea12b9aedae7df762
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-07-18 14:58:44 -0700

    Refactor.

--- lab-nathan/lib/fp.js
+++ lab-nathan/lib/fp.js
@@ -3,22 +3,10 @@
 let fp = {};
 module.exports = fp;
 
-fp.map = (array, func) => {
-  return Array.prototype.map.call(array, func);
-};
-
-fp.filter = (array, predicate) => {
-  return Array.prototype.filter.call(array, predicate);
-};
-
-fp.reduce = (array, func, initialValue) => {
-  return Array.prototype.reduce.call(array, func, initialValue);
-};
-
-fp.concat = (arrayA, arrayB) => {
-  return Array.prototype.concat.call(arrayA, arrayB);
-};
-
+fp.map = (array, func) => Array.prototype.map.call(array, func);
+fp.filter = (array, predicate) => Array.prototype.filter.call(array, predicate);
+fp.reduce = (array, func, initialValue) => Array.prototype.reduce.call(array, func, initialValue);
+fp.concat = (arrayA, arrayB) => Array.prototype.concat.call(arrayA, arrayB);
 fp.splice = (array, start, deleteCount, items) => {
   Array.prototype.splice.call(array, start, deleteCount, ...items);
   return array;
--- lab-nathan/test/fp-test.js
+++ lab-nathan/test/fp-test.js
@@ -5,6 +5,10 @@ const expect = require('chai').expect;
 
 describe('fp', () => {
   describe('#map()', () => {
+    it('should fail with invalid inputs.', () => {
+      expect(fp.map).to.throw(Error);
+    });
+
     it('should return an array of doubled values.', () => {
       let result = fp.map([1, 2, 3], num => num * 2);
       expect(result).to.deep.equal([2, 4, 6]);
@@ -12,6 +16,10 @@ describe('fp', () => {
   });
 
   describe('#filter()', () => {
+    it('should fail with invalid inputs.', () => {
+      expect(fp.filter).to.throw(Error);
+    });
+
     it('should return 2.', () => {
       let result = fp.filter([1, 2, 3], num => num !== 2);
       expect(result).to.deep.equal([1, 3]);
@@ -19,6 +27,10 @@ describe('fp', () => {
   });
 
   describe('#reduce()', () => {
+    it('should fail with invalid inputs.', () => {
+      expect(fp.reduce).to.throw(Error);
+    });
+
     it('should return 6.', () => {
       let result = fp.reduce([1, 2, 3], (acc, cur) => acc + cur, 0);
       expect(result).to.equal(6);
@@ -26,6 +38,10 @@ describe('fp', () => {
   });
 
   describe('#concat()', () => {
+    it('should fail with invalid inputs.', () => {
+      expect(fp.concat).to.throw(Error);
+    });
+
     it('should return [1, 2, 3, 4, 5, 6].', () => {
       let result = fp.concat([1, 2, 3], [4, 5, 6]);
       expect(result).to.deep.equal([1, 2, 3, 4, 5, 6]);
@@ -33,6 +49,10 @@ describe('fp', () => {
   });
 
   describe('#splice()', () => {
+    it('should fail with invalid inputs.', () => {
+      expect(fp.splice).to.throw(Error);
+    });
+
     it('should return [1, 2, 3, 7, 8, 9, 4, 5, 6].', () => {
       let result = fp.splice([1, 2, 3, 4, 5, 6], 3, 0, [7, 8, 9]);
       expect(result).to.deep.equal([1, 2, 3, 7, 8, 9, 4, 5, 6]);
